87204e342709dfd98fe37faa7b147784ca962663
Danny Skoog <danny.skoog@voiapp.io>
2022-05-11 12:13:38 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DeckGL from '@deck.gl/react';
+import {LineLayer} from '@deck.gl/layers';
+import {StaticMap} from 'react-map-gl';
+
+// Set your mapbox access token here
+const MAPBOX_ACCESS_TOKEN = 'your_mapbox_token';
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [
+  {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
+];
 
 function App() {
+  const getLayers = (id: string) => [
+    new LineLayer({id: 'line-layer', data})
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>learn react</h1>
+      {/* Without children */}
+      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={getLayers('1')} />
+
+      {/* With single child */}
+      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={getLayers('2')}>
+        <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+      </DeckGL>
+
+      {/* With multiple children */}
+      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={getLayers('3')}>
+        <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+        <div>Lorem ipsum</div>
+      </DeckGL>
+    </>
   );
 }
 
-export default App;
+export default App
